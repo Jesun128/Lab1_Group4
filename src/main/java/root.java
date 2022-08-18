79f7aeb980e2e326377527e6788b8027e13fa209
Jesun128 <111490001+Jesun128@users.noreply.github.com>
2022-08-18 15:17:21 +0800

    Update and rename TestClass2.java to root.java

---
+++ src/main/java/root.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class root {
+        public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input:");
+        int n = sc.nextInt();
+        square(n);
+        }
+        static int square(int n){ 
+            if(n<=0){
+                return 0;
+            }
+        int i = n*n;
+        System.out.println("Square root of: " + n + "=" + i);
+        return square(n-1);
+        }
+    
